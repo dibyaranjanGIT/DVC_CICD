dfdcf4d26681d2114ae610318c34bf1ec73a08d5
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-01-03 20:48:58 +0530

    Changes done on stage1_load_save file

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -1,8 +1,13 @@
 import yaml
 import os
 
-def read_yaml(path_to_yaml: str):
+def read_config(path_to_yaml: str):
     with open(path_to_yaml) as yaml_file:
         content = yaml.safe_load(yaml_file)
 
     return content
+
+def create_directory(dirs: list):
+    for dir_path in dirs:
+        os.makedirs(dir_path, exist_ok=True)
+        print(f"{dir_path} is created")
---
+++ stage1_load_save.py
@@ -0,0 +1,30 @@
+from src.utils.all_utils import read_config,create_directory
+import argparse
+import pandas as pd
+import os
+
+def get_data(config_path):
+    config = read_config(config_path)
+    remote_data_path = config["data_source"]
+    df = pd.read_csv(remote_data_path, sep=";")
+    # print(config)
+    # print(df.head())
+
+    # Save data set in local
+    artifcats_dir = config["artifacts"]["artifcats_dir"]
+    raw_file_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+
+    raw_local_dir_path = os.path.join(artifcats_dir, raw_file_dir)
+    create_directory(dirs=[raw_local_dir_path])
+
+    raw_local_file_path = os.path.join(raw_local_dir_path, raw_local_file)
+
+    df.to_csv(raw_local_file_path, sep=";", index=False)
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configuration/config.yaml")
+    parsed_args = args.parse_args()
+
+    get_data(config_path=parsed_args.config)
