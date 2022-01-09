2ea7b6b75a9c999f2683b3f894b0b33281165ac8
Dibyaranjan Jena <45364252+dibyaranjanGIT@users.noreply.github.com>
2022-01-09 21:03:40 +0530

    Created split_data file

--- src/utils/all_utils.py
+++ src/utils/all_utils.py
@@ -11,3 +11,7 @@ def create_directory(dirs: list):
     for dir_path in dirs:
         os.makedirs(dir_path, exist_ok=True)
         print(f"{dir_path} is created")
+
+def save_local_df(data, data_path, index_status=False):
+    data.to_csv(data_path, index_status)
+    print(f"data is save at {data_path}")
---
+++ stage2_split_data.py
@@ -0,0 +1,51 @@
+from src.utils.all_utils import read_config, create_directory
+import argparse
+import pandas as pd
+import os
+from sklearn.model_selection import train_test_split
+
+def split_save(config_path, params_path):
+    config = read_config(config_path)
+    params = read_config(params_path)
+    # remote_data_path = config["data_source"]
+
+    # Save data set in local
+    artifcats_dir = config["artifacts"]["artifcats_dir"]
+    raw_file_dir = config["artifacts"]["raw_local_dir"]
+    raw_local_file = config["artifacts"]["raw_local_file"]
+    split_data_dir = config["artifacts"]["split_data_dir"]
+    split_data_train= config["artifacts"]["train"]
+    split_data_test = config["artifacts"]["test"]
+
+    raw_local_dir = os.path.join(artifcats_dir,raw_file_dir)
+    raw_local_dir_path = os.path.join(raw_local_dir, raw_local_file)
+
+    df = pd.read_csv(raw_local_dir_path, sep=";")
+
+
+    split_ratio = params["base"]["test_size"]
+    random_state = params["base"]["random_state"]
+
+    train, test = train_test_split(df, test_size=split_ratio, random_state=random_state)
+
+    create_directory([os.path.join(artifcats_dir, split_data_dir),
+                      os.path.join(artifcats_dir, split_data_dir)])
+
+    train_data_path1 = os.path.join(artifcats_dir, split_data_dir)
+    train_data_path2 = os.path.join(train_data_path1, split_data_train)
+    print(train_data_path2)
+    print(train.head())
+    train.to_csv(train_data_path2, sep=";", index=False)
+
+    test_data_path1 = os.path.join(artifcats_dir, split_data_dir)
+    test_data_path2 = os.path.join(test_data_path1, split_data_test)
+    test.to_csv(test_data_path2, sep=";", index=False)
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", "-c", default="configuration/config.yaml")
+    args.add_argument("--params", "-p", default="params.yaml")
+    parsed_args = args.parse_args()
+
+    split_save(config_path=parsed_args.config, params_path=parsed_args.params)
